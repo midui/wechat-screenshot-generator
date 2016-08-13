8d664a400b8cf6340a8f15fd9c1f92944507cf20
xcatliu <xcatliu@gmail.com>
2016-08-13 20:51:05 +0800

    Update deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,9 +5,9 @@ git remote add coding git@git.coding.net:xcatliu/wechat-screenshot-generator.git
 git checkout master
 git push coding master
 
-rm -rf .deploy_gh_pages
-git clone git@github.com:open-ideas-club/wechat-screenshot-generator.git .deploy_gh_pages
-cd .deploy_gh_pages
+rm -rf .deploy
+git clone git@github.com:open-ideas-club/wechat-screenshot-generator.git .deploy
+cd .deploy
 
 git checkout gh-pages
 
@@ -16,7 +16,7 @@ rsync -a ../public/ ./
 git add --all
 git commit -m "Update gh-pages"
 git push origin gh-pages
-git push coding coding-pages
+git push coding gh-pages:coding-pages
 
 cd ..
-rm -rf .deploy_gh_pages
+rm -rf .deploy
