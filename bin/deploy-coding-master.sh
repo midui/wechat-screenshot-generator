b33f814ac82744ac6dbc1e3c832b166381050fa0
xcatliu <xcatliu@gmail.com>
2016-08-13 20:40:49 +0800

    Update travis

--- bin/deploy-coding-master.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-set -e
-
-git checkout master
-
-git remote add coding git@git.coding.net:xcatliu/wechat-screenshot-generator.git
-git push coding master
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 set -e
 
+git remote add coding git@git.coding.net:xcatliu/wechat-screenshot-generator.git
+
+git checkout master
+git push coding master
+
 rm -rf .deploy_gh_pages
 git clone git@github.com:open-ideas-club/wechat-screenshot-generator.git .deploy_gh_pages
 cd .deploy_gh_pages
@@ -12,6 +17,7 @@ rsync -a ../public/ ./
 git add --all
 git commit -m "Update gh-pages"
 git push origin gh-pages
+git push coding coding-pages
 
 cd ..
 rm -rf .deploy_gh_pages
