63cae99413ea745c85df60905bf22c8eca700208
xcatliu <xcatliu@gmail.com>
2016-08-22 15:48:51 +0800

    Update deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,33 +3,39 @@ set -e
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then
-    echo "Skipping deploy; just doing a build."
-    npm test
+    echo "Skipping deploy, just doing a build."
+    # npm test
     npm run build
     exit 0
 fi
 
-npm test
+# npm test
 npm run build
 
+# Sync master to coding.net
 git remote add coding git@git.coding.net:xcatliu/wechat-screenshot-generator.git
-
 git checkout master
 git push coding master
 
+# Get latest commit hash of master
+LATEST_HASH=$(git log -n 1 --pretty=format:"%H")
+
+# Clone a new repo to .deploy
 rm -rf .deploy
 git clone git@github.com:hack1day/wechat-screenshot-generator.git .deploy
-
 cd .deploy
 git remote add coding git@git.coding.net:xcatliu/wechat-screenshot-generator.git
 
+# Copy all build results to .deploy
 git checkout gh-pages
-
+git pull origin gh-pages
+find -maxdepth 1 ! -name '.' ! -name '.git' ! -name '.gitignore' | xargs rm -r
 rsync -a ../public/ ./
 
+# Push if has change
 if [ -n "$(git status --porcelain)" ]; then
   git add --all
-  git commit -m "Update gh-pages"
+  git commit -m "Update to $LATEST_HASH"
   git push origin gh-pages
   git push coding gh-pages:coding-pages
 else
